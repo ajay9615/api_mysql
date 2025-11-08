e28ef3664a5c08207c073cd22c46bb38905f6c19
LearningFlutter373 <155526378+LearningFlutter373@users.noreply.github.com>
2025-11-08 06:48:34 +0530

    i changed some codes in apis

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "schoolmanagement"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((error) => {
     if (error) {
         console.log("Database connection failed");
-    }
-    else {
-        console.log("database connected successfully");
+    } else {
+        console.log("Database connected successfully");
     }
 });
-module.exports = db;
